b8de9dcf7fea451070ccfed1918542475c791bc9
WebStudio523 <webstudio523@gmail.com>
2019-05-05 18:55:05 +0800

    followup

--- src/js/content/followup.js
+++ src/js/content/followup.js
@@ -1,5 +1,18 @@
+//Followup Element JS File
 module.exports = {
-    createElement() {
-        return "<button>OK</button>";
+    create() {
+        var el = document.createElement("div");
+        el.setAttribute("class", "dropdown");
+        el.innerHTML = this.renderHTML();
+        el.children[1].addEventListener("click", function() { //Add actoion when clicking followup button.
+            console.log(this.parentElement.children[0]);
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            this.parentElement.children[0].classList.toggle("show");
+        });
+        return el;
+    },
+
+    renderHTML() {
+        return "<div id='myDropdown' class='dropdown-content' style='top:-178px; width:200px'><a href='#'>Once per day</a><a href='#'>Once every two days</a><a href='#'>Once every three days</a><a href='#'>Once per week</a></div><button class='dropbtn'>Followup</button>";
     },
 }
--- src/js/content_script.js
+++ src/js/content_script.js
@@ -3,26 +3,38 @@ import * as followup from "./content/followup.js";
 
 var content = {
     init: function() {
-        InboxSDK.load(2, "sdk_serkan_47dea3822e").then(function(sdk) {
-            //Import Inboxsdk for gmail extension
-            sdk.Compose.registerComposeViewHandler(function(composeView) {
-                //Add bar under the Gmail Default Control Bar
-                addContent(composeView);
+        window.onclick = function(event) {
+                if (!event.target.matches('.dropbtn')) {
+                    var dropdowns = document.getElementsByClassName("dropdown-content");
+                    var i;
+                    for (i = 0; i < dropdowns.length; i++) {
+                        var openDropdown = dropdowns[i];
+                        if (openDropdown.classList.contains('show')) {
+                            openDropdown.classList.remove('show');
+                        }
+                    }
+                }
+            },
+
+            InboxSDK.load(2, "sdk_serkan_47dea3822e").then(function(sdk) {
+                //Import Inboxsdk for gmail extension
+                sdk.Compose.registerComposeViewHandler(function(composeView) {
+                    addContent(composeView);
+                });
             });
-        });
     }
 }
 var myContent = Object.create(content);
 myContent.init();
 
+//Add bar under the Gmail Default Control Bar
 function addContent(composeView) {
     var statusBarView = composeView.addStatusBar({
         height: "10",
         el: el
     })
     var el = document.createElement("div");
-    el.className += "followup";
+    el.setAttribute("class", "followup")
     statusBarView.el = el;
-    console.log(followup.createElement());
-    statusBarView.el.innerHTML = followup.createElement();
+    statusBarView.el.appendChild(followup.create());
 }
