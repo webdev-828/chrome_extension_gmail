7575dbb9b8b2f659fe065c205c2547629a06c861
WebStudio523 <webstudio523@gmail.com>
2019-05-05 08:31:51 +0800

    followup

--- src/js/content_script.js
+++ src/js/content_script.js
@@ -1,3 +1,28 @@
 import "../css/content.css";
+import * as followup from "./content/followup.js";
 
-alert("OK")
+var content = {
+    init: function() {
+        InboxSDK.load(2, "sdk_serkan_47dea3822e").then(function(sdk) {
+            //Import Inboxsdk for gmail extension
+            sdk.Compose.registerComposeViewHandler(function(composeView) {
+                //Add bar under the Gmail Default Control Bar
+                addContent(composeView);
+            });
+        });
+    }
+}
+var myContent = Object.create(content);
+myContent.init();
+
+function addContent(composeView) {
+    var statusBarView = composeView.addStatusBar({
+        height: "10",
+        el: el
+    })
+    var el = document.createElement("div");
+    el.className += "followup";
+    statusBarView.el = el;
+    console.log(followup.createElement());
+    statusBarView.el.innerHTML = followup.createElement();
+}
